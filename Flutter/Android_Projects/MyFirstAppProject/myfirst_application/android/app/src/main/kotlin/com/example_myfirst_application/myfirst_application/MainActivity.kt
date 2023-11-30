610c6bc065ff99aba47bcdfadcb790354d93d1aa
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-11-30 12:29:12 +0530

    .

--- Flutter/Android_Projects/MyFirstAppProject/myfirst_application/android/app/src/main/kotlin/com/example_myfirst_application/myfirst_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example_myfirst_application.myfirst_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter/Android_Projects/notes/android/app/src/main/kotlin/com/example_notes/notes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example_notes.notes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
